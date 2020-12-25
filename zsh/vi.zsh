5c39e81bac7888e72eca05148d4c1c66cb6ebb35
logicskky <logicskky@163.com>
2020-12-25 21:12:01 +0800

    Always use a block shape cursor.

--- zsh/vi.zsh
+++ zsh/vi.zsh
@@ -1,8 +1,6 @@
 # My Zsh Configuration -- Vi Mode
 
 bindkey -v
-bindkey -M vicmd "H" vi-beginning-of-line
-bindkey -M vicmd "L" vi-end-of-line
 bindkey -M vicmd "u" undo
 
 #function zle-line-init zle-keymap-select {
@@ -11,30 +9,32 @@ bindkey -M vicmd "u" undo
 #    zle reset-prompt
 #}
 
-function zle-keymap-select {
-    if [[ ${KEYMAP} == vicmd ]] || [[ $1 = 'block' ]]; then
-        echo -ne '\e[1 q'
-    elif [[ ${KEYMAP} == main ]] || [[ ${KEYMAP} == viins ]] || [[ ${KEYMAP} = '' ]] || [[ $1 = 'beam' ]]; then
-        echo -ne '\e[5 q'
-    fi
-}
-zle -N zle-keymap-select
+# - Use different shapes of cursor in different modes. -
 
-# Use beam shape cursor on startup.
-echo -ne '\e[5 q'
-
-# Use beam shape cursor for each new prompt.
-preexec() {
-    echo -ne '\e[5 q'
-}
-
-_fix_cursor() {
-    echo -ne '\e[5 q'
-}
-precmd_functions+=(_fix_cursor)
-
-#zle -N zle-line-init
+##function zle-keymap-select {
+#    if [[ ${KEYMAP} == vicmd ]] || [[ $1 = 'block' ]]; then
+#        echo -ne '\e[1 q'
+#    elif [[ ${KEYMAP} == main ]] || [[ ${KEYMAP} == viins ]] || [[ ${KEYMAP} = '' ]] || [[ $1 = 'beam' ]]; then
+#        echo -ne '\e[5 q'
+#    fi
+#}
 #zle -N zle-keymap-select
-
-KEYTIMEOUT=1
+#
+## Use beam shape cursor on startup.
+#echo -ne '\e[5 q'
+#
+## Use beam shape cursor for each new prompt.
+#preexec() {
+#    echo -ne '\e[5 q'
+#}
+#
+#_fix_cursor() {
+#    echo -ne '\e[5 q'
+#}
+#precmd_functions+=(_fix_cursor)
+#
+##zle -N zle-line-init
+##zle -N zle-keymap-select
+#
+#KEYTIMEOUT=1
 
